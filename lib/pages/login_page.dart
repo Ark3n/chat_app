6efbaeac430323b83cce2d509244b699036575c7
Arken <Arken.Sarsenov@outlook.com>
2025-02-06 23:28:15 +0500

    UI: login_page has been created

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,87 @@
+import 'package:chat_app/components/my_button.dart';
+import 'package:chat_app/components/my_text_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatelessWidget {
+  // email and pw controller
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  LoginPage({super.key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
+  // methods
+  void login() {
+    debugPrint('login tapped');
+  }
 
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 50),
+
+            // welcome back message
+            Text(
+              'Welcome back, you\'ve been missed!',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 25),
+
+            // email text field
+            MyTextField(
+              hintText: 'Email',
+              obscureText: false,
+              controller: _emailController,
+            ),
+            const SizedBox(height: 20),
+
+            // pw text field
+            MyTextField(
+              hintText: 'Password',
+              obscureText: true,
+              controller: _pwController,
+            ),
+            const SizedBox(height: 25),
+
+            // login button
+            MyButton(
+              title: 'Log In',
+              onTap: login,
+            ),
+            const SizedBox(height: 25),
+
+            // register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not a member? ',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+                Text(
+                  'Register now',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
